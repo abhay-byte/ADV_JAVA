3879f0943c4c0e2d6813064878f241fd3bd7a99d
abhay-byte <abhay02delhi@gmail.com>
2025-05-05 08:35:52 +0530

    program18 updated

--- program18/WEB-INF/classes/GetCookieServlet.java
+++ program18/WEB-INF/classes/GetCookieServlet.java
@@ -1,5 +1,37 @@
-package program18.WEB-INF.classes;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class GetCookieServlet {
-    
+@WebServlet("/GetCookieServlet")
+public class GetCookieServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        Cookie[] cookies = request.getCookies();
+        boolean found = false;
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("username")) {
+                    out.println("<h3>Stored Cookie Value: " + cookie.getValue() + "</h3>");
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!found) {
+            out.println("<h3>No cookies found!</h3>");
+        }
+
+        out.println("<h3>Abhay Raj, 00976803122</h3>");
+    }
 }
--- program18/WEB-INF/classes/SetCookieServlet.java
+++ program18/WEB-INF/classes/SetCookieServlet.java
@@ -1,5 +1,27 @@
-package program18.WEB-INF.classes;
 
-public class SetCookieServlet {
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SetCookieServlet")
+public class SetCookieServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        Cookie userCookie = new Cookie("username", "AdminUser");
+        userCookie.setMaxAge(60 * 60 * 24); // 1 day
+        response.addCookie(userCookie);
+
+        out.println("<h3>Cookie 'username' has been set!</h3>");
+        out.println("<a href='GetCookieServlet'>Click here to retrieve the cookie</a>");
+        out.println("<h3>Abhay Raj, 00976803122</h3>");
+    }
 }
